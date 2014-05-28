xolox/python-deb-pkg-tools

commit 5bf01b09f0c57225d7a2b776866872184898af57

Sanity check arguments in deb_pkg_tools.package.find_latest_version()

deb_pkg_tools/package.py
@@ -1,7 +1,7 @@
 # Debian packaging tools: Package manipulation.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: May 25, 2014
+# Last Change: May 26, 2014
 # URL: https://github.com/xolox/python-deb-pkg-tools
 
 """
@@ -27,7 +27,7 @@ import tempfile
 # External dependencies.
 from debian.deb822 import Deb822
 from executor import execute
-from humanfriendly import format_path, pluralize
+from humanfriendly import concatenate, format_path, pluralize
 
 # Modules included in our package.
 from deb_pkg_tools.control import (deb822_from_string,
@@ -186,15 +186,21 @@ def collect_related_packages(filename):
 
 def find_latest_version(packages):
     """
-    Find the package archive with the highest version number. Uses ``dpkg
-    --compare-versions ...`` for version comparison.
+    Find the package archive with the highest version number. Uses
+    :py:class:`deb_pkg_tools.version.Version` objects for version comparison.
+    Raises :py:exc:`ValueError` when not all of the given package archives
+    share the same package name.
 
     :param packages: A list of filenames (strings) and/or
                      :py:class:`PackageFile` objects.
     :returns: The :py:class:`PackageFile` with
               the highest version number.
     """
-    return sorted(map(parse_filename, packages))[-1]
+    packages = list(map(parse_filename, packages))
+    names = sorted(set(p.name for p in packages))
+    if len(names) > 1:
+        raise ValueError("Refusing to compare unrelated packages! (%s)" % concatenate(names))
+    return packages[-1]
 
 def inspect_package(archive):
     """

commit 9de35116957311b650593977753ff70d690c130d

Bug fix for find_latest_version() introduced in commit 5bf01b0
See also: https://travis-ci.org/xolox/python-deb-pkg-tools/jobs/26247681

deb_pkg_tools/__init__.py
@@ -11,7 +11,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Semi-standard module versioning.
-__version__ = '1.18.4'
+__version__ = '1.18.5'
 
 # The following non-essential Debian packages need to be
 # installed in order for deb-pkg-tools to work properly.

deb_pkg_tools/package.py
@@ -1,7 +1,7 @@
 # Debian packaging tools: Package manipulation.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: May 26, 2014
+# Last Change: May 28, 2014
 # URL: https://github.com/xolox/python-deb-pkg-tools
 
 """
@@ -197,10 +197,11 @@ def find_latest_version(packages):
     :returns: The :py:class:`PackageFile` with
               the highest version number.
     """
-    packages = list(map(parse_filename, packages))
-    names = sorted(set(p.name for p in packages))
+    packages = sorted(map(parse_filename, packages))
+    names = set(p.name for p in packages)
     if len(names) > 1:
-        raise ValueError("Refusing to compare unrelated packages! (%s)" % concatenate(names))
+        msg = "Refusing to compare unrelated packages! (%s)"
+        raise ValueError(msg % concatenate(sorted(names)))
     return packages[-1]
 
 def inspect_package(archive):