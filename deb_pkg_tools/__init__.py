803e5e3fbae634a3e133713af43a21e94a40f8df
Peter Odding <peter@peterodding.com>
2016-11-18 04:03:57 +0100

    Release 2.0: Stop using system wide temporary directory (concurrent builds!)

--- deb_pkg_tools/__init__.py
+++ deb_pkg_tools/__init__.py
@@ -1,11 +1,11 @@
 # Debian packaging tools.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: November 17, 2016
+# Last Change: November 18, 2016
 # URL: https://github.com/xolox/python-deb-pkg-tools
 
 # Semi-standard module versioning.
-__version__ = '1.37'
+__version__ = '2.0'
 
 debian_package_dependencies = (
     'apt',        # apt-get
