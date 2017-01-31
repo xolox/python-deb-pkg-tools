c5ab2f6682558d9bca54298c0e09a3974d8da341
Peter Odding <peter@peterodding.com>
2017-01-31 23:20:05 +0100

    Release 4.0: Support for architecture restriction parsing (#9)
    I'm bumping the major version number because 754debc0b61 removed the
    deb_pkg_tools.{compat.total_ordering,utils.OrderedObject} classes and
    internal methods like _key() so strictly speaking this breaks backwards
    compatibility, however both of these classes were part of miscellaneous
    scaffolding used by deb-pkg-tools but not an intentional part of the
    documented API, so I don't expect this to be particularly relevant
    to most (if not all) users of deb-pkg-tools.

--- deb_pkg_tools/__init__.py
+++ deb_pkg_tools/__init__.py
@@ -1,7 +1,7 @@
 # Debian packaging tools.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: January 27, 2017
+# Last Change: January 31, 2017
 # URL: https://github.com/xolox/python-deb-pkg-tools
 
 """
@@ -13,7 +13,7 @@ the `deb-pkg-tools` package.
 """
 
 # Semi-standard module versioning.
-__version__ = '3.1'
+__version__ = '4.0'
 
 debian_package_dependencies = (
     'apt',        # apt-get
